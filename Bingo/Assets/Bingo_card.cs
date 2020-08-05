057f1a515050017b0ef0ac5f03f059d8679676c5
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-08-05 17:55:13 -0400

    attempted duplication

---
+++ Bingo/Assets/Bingo_card.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bingo_card : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button zero_button;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(zero_button, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
